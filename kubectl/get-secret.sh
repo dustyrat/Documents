4f39e96d85c6b2fbe6ba3ae33f4b97b6c08850d6
Dustin Ratcliffe <dustin.k.ratcliffe@gmail.com>
2021-09-08 15:14:56 -0500

    Update get-secret.sh

--- kubectl/get-secret.sh
+++ kubectl/get-secret.sh
@@ -6,13 +6,14 @@
 
 get-secret(){
     echo "Secret: $1 ($2)"
-    mkdir -p $1;
-    cd $1;
+    directory="$2/$1"
+    mkdir -p $directory;
+    cd $directory;
     kubectl get secret $1 -o yaml --context $2 > secret.yaml;
     mkdir -p data;
     yq e -M ".data" secret.yaml | while read d; do
         eval $(echo $d | awk -F': ' '{ print "key="$1, "value="$2 }');
-        echo $value | base64 -d > "data/$key";
+        echo $value | base64 -D > "data/$key";
     done;
     cd ../;
 }
