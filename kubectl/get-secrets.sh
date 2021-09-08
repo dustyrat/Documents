56d327424d920e564fe52370b7a30d3b6deab8b3
Dustin Ratcliffe <dustin.k.ratcliffe@gmail.com>
2021-09-08 15:14:15 -0500

    Update get-secrets.sh

--- kubectl/get-secrets.sh
+++ kubectl/get-secrets.sh
@@ -5,6 +5,12 @@
 ## - yq: https://github.com/mikefarah/yq
 
 get-secrets(){
+    echo "Deployment: $1 ($2)";
+
+    kubectl get deploy $1 -o yaml --context $2 | yq e -M ".spec.template.spec.containers[].env[].valueFrom.secretKeyRef.name" - | sort | uniq | while read secret; do
+        get-secret.sh $secret $2;
+    done;
+
     kubectl get deploy $1 -o yaml --context $2 | yq e -M ".spec.template.spec.volumes[].secret.secretName" - | while read secret; do
         get-secret.sh $secret $2;
     done;
