1d1c9103c3cc16268dabaa802493819c5afabdfc
Dustin Ratcliffe <dustin.k.ratcliffe@gmail.com>
2022-04-21 15:54:38 -0500

    Update get-secrets.sh

--- kubectl/get-secrets.sh
+++ kubectl/get-secrets.sh
@@ -2,17 +2,20 @@
 
 ## Requirements:
 ## - kubectl: https://kubernetes.io/docs/tasks/tools/
-## - yq: https://github.com/mikefarah/yq
 
 get-secrets(){
-    echo "Deployment: $1 ($2)";
+    deployment=$1
+    context=$2
 
-    kubectl get deploy $1 -o yaml --context $2 | yq e -M ".spec.template.spec.containers[].env[].valueFrom.secretKeyRef.name" - | sort | uniq | while read secret; do
-        get-secret.sh $secret $2;
-    done;
-
-    kubectl get deploy $1 -o yaml --context $2 | yq e -M ".spec.template.spec.volumes[].secret.secretName" - | while read secret; do
-        get-secret.sh $secret $2;
+    echo "Deployment: $deployment ($context)";
+    kubectl get deploy $deployment --context $context -o jsonpath="\
+{range .spec.template.spec.containers[*]}{range .env[*]}{.valueFrom.secretKeyRef.name}{'\n'}{end}{end}\
+{range .spec.template.spec.containers[*]}{range .envFrom[*]}{.secretRef.name}{'\n'}{end}{end}\
+{range .spec.template.spec.imagePullSecrets[?(@.name!='regsecret')]}{.name}{'\n'}{end}\
+{range .spec.template.spec.volumes[*]}{.secret.secretName}{'\n'}{end}\
+{range .spec.tls[*]}{.secretName}{'\n'}{end}\
+    " | sort | uniq | xargs -n1 | while read secret; do
+        get-secret.sh $secret $context;
     done;
 }
 
